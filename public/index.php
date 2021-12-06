09dc013deb8856ac836995e0f14cdf33ba5a1c0e
yuanzhumc <yuanzhumc@163.com>
2021-12-06 16:06:29 +0800

    develop 重新构建框架-升级至thinkphp6使用easy-admin后台框架

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -10,8 +10,24 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+namespace think;
 
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+// 声明全局变量
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', __DIR__ . DS . '..' . DS);
+
+// 判断是否安装程序
+if (!is_file(ROOT_PATH . 'config' . DS . 'install' . DS . 'lock' . DS . 'install.lock')) {
+    exit(header("location:/install.php"));
+}
+
+// 执行HTTP应用并响应
+$http = (new App())->http;
+
+$response = $http->run();
+
+$response->send();
+
+$http->end($response);
--- public/router.php
+++ public/router.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2019 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -13,8 +13,5 @@
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     return false;
 } else {
-    if (!isset($_SERVER['PATH_INFO'])) {
-        $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
-    }
     require __DIR__ . "/index.php";
 }
